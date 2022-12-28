be18d8ce67b1d86099eed89b53c1efce2982ccb0
farid fadilah permana <faridfadillah29@gmail.com>
2022-12-28 15:15:19 +0700

    updated

--- app/Helpers/HttpClient.php
+++ app/Helpers/HttpClient.php
@@ -7,10 +7,15 @@ use Illuminate\Support\Facades\Http;
 class HttpClient{
   static function fetch($method, $url, $body = [], $files = []){
     // Jika method == Get, kembalikan response dengan method get
+    $headers = [];
+    $token = session()->get('token', '');
+    if($token != ""){
+      $headers['authorization'] = 'Bearer ' . $token;
+    }
     if($method == "GET") return Http::get($url)->json([]);
     // Jika terdapat file, client berupa multipart
     if(sizeof($files) > 0){
-      $client = Http::asMultipart();
+      $client = Http::asMultipart()->withHeaders($headers);
       // attach file pada clients
       foreach($files as $key => $file){
         $path = $file->getPathname();
